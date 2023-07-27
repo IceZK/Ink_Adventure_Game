4cae11d23a93b3f4bb1a5c8120db273f47fd6751
Kagerou05 <auphaiphat_l@cmu.ac.th>
2023-07-27 15:10:24 +0700

    Update Fighting System

--- Assets/Game_Assets/Code/FightingSystem.cs
+++ Assets/Game_Assets/Code/FightingSystem.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 namespace Kagerou
 {
     public class FightingSystem : MonoBehaviour
     {
-        private int phase = 0;
+        [SerializeField] protected int phase = 0;
+        public Button action_1;
+        public Button action_2;
+        public Button action_3;
+        public Button action_4;
         // Start is called before the first frame update
         void Start()
         {
@@ -15,16 +22,47 @@ namespace Kagerou
         // Update is called once per frame
         void Update()
         {
-            phase = PhaseCounter();
             Debug.Log(phase);
         }
-            
-        private int PhaseCounter()
+        public void OnClick()
         {
-            int turn = phase;
-            if (turn < 4) turn++;
-            else turn = 0;
-            return turn;
+            ActionPhase();
+            Debug.Log("Click");
+        }
+        
+        private void ActionPhase()
+        {
+            switch(phase) 
+            {
+                case 0:AttackSelectPhase();
+                    break;
+                case 1:AttackAnimatePhase();
+                    break;
+                case 2:DefenseSelectPhase();
+                    break;
+                case 3:DefenseAnimatePhase();
+                    break;
+            }
+        }
+        private void AttackSelectPhase()
+        {
+            Debug.Log("AttackSelectPhase");
+            phase++;
+        }
+        private void AttackAnimatePhase()
+        {
+            Debug.Log("AttackAnimationPhase");
+            phase++;
+        }
+        private void DefenseSelectPhase()
+        {
+            Debug.Log("DefenseSelectPhase");
+            phase++;
+        }
+        private void DefenseAnimatePhase()
+        {
+            Debug.Log("DefenseAnimationPhase");
+            phase++;
         }
     }
 }
