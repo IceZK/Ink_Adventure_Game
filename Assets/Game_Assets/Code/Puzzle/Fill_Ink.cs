16efd82b18799af7ff649a81081e8151d114bc03
IceZK <ice.cm1@gmail.com>
2023-08-03 11:21:31 +0700

    Trap Script

---
+++ Assets/Game_Assets/Code/Puzzle/Fill_Ink.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fill_Ink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
